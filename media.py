9a2b46f12b50262477e94fd2b6f15341eee3a961
yachen <ya.chen@excelsiortech.cn>
2017-08-21 13:07:45 +0800

    delete useless comments

--- media.py
+++ media.py
@@ -1,10 +1,5 @@
 # Mini-Project: Movies Website
 
-# In this file, you will define the class Movie. You could do this
-# directly in entertainment_center.py but many developers keep their
-# class definitions separate from the rest of their code. This also
-# gives you practice importing Python files.
-
 
 class Movie():
     # This class provides a way to store movie related information
